acdf7b684187b76eb7605d8717c8df67371a2512
Totalpolarfox <totalpolarfox@outlook.com>
2024-01-30 10:13:43 +0300

    Updated def main()

--- seminar_8_dz.py
+++ seminar_8_dz.py
@@ -39,16 +39,39 @@ def copy_data():
 
 # Код действий пользователя
 def main():
-    print()
-    print('\u001b[4mВыберите необходимое действие:\u001b[0m')
-    print('\u001b[32m 0 \u001b[0m - Выход из программы')
-    print('\u001b[32m 1 \u001b[0m - Показать все контакты')
-    print('\u001b[32m 2 \u001b[0m - Поиск контакта')
-    print('\u001b[32m 3 \u001b[0m - Сохранить новый контакт')
-    print('\u001b[32m 4 \u001b[0m - Редактировать контакт')
-    print('\u001b[32m 5 \u001b[0m - Удалить контакт')
-    print('\u001b[32m 6 \u001b[0m - Сохранить контакт в новый файл')
-    print()
+    file_name = 'phone_book.txt'
+    flag = True
+    while flag:
+        print()
+        print('\u001b[32m 0 \u001b[0m - Выход из программы')
+        print('\u001b[32m 1 \u001b[0m - Показать все контакты')
+        print('\u001b[32m 2 \u001b[0m - Поиск контакта')
+        print('\u001b[32m 3 \u001b[0m - Сохранить новый контакт')
+        print('\u001b[32m 4 \u001b[0m - Редактировать контакт')
+        print('\u001b[32m 5 \u001b[0m - Удалить контакт')
+        print('\u001b[32m 6 \u001b[0m - Сохранить контакт в новый файл')2
+        print()
+        answer = input('\u001b[4mВыберите необходимое действие:\u001b[0m ')
+        if answer == 0:
+            flag == False
+        elif answer == 1:
+            data = read_data(file_name)
+            show_data(data)
+        elif answer == 2:
+            data = read_data(file_name)
+            founded_data = search_data(data)
+            show_data(founded_data)
+        elif answer == 3:
+            write_data(file_name)
+        elif answer == 4:
+            data = read_data(file_name)
+            edit_data(data)
+        elif answer == 5:
+            data = read_data(file_name)
+            del_data(data)
+        elif answer == 6:
+            data = read_data(file_name)
+            copy_data(data)
 
 if __name__ == '__main__':
     main()
